c2a69fefed69eda9b422c1a2fd91b3998f96f185
Joeeeeca <joechapon101@gmail.com>
2025-12-05 16:49:21 +0000

    Making a backup for pb_data

---
+++ backend/pb_migrations/1764952865_updated_posts.js
@@ -0,0 +1,86 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1125843985")
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1591429585",
+    "max": 0,
+    "min": 0,
+    "name": "excerpt",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3089781476",
+    "max": 0,
+    "min": 0,
+    "name": "slugs",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text105650625",
+    "max": 0,
+    "min": 0,
+    "name": "category",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(9, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text320982995",
+    "max": 0,
+    "min": 0,
+    "name": "readTime",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1125843985")
+
+  // remove field
+  collection.fields.removeById("text1591429585")
+
+  // remove field
+  collection.fields.removeById("text3089781476")
+
+  // remove field
+  collection.fields.removeById("text105650625")
+
+  // remove field
+  collection.fields.removeById("text320982995")
+
+  return app.save(collection)
+})
